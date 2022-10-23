4dc5045987f94c8006cd8527523dfa9d190e1201
yuuuba <nsabu45@gmail.com>
2022-10-24 00:29:30 +0900

    viewページ動作するまで #1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,26 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save!
+      redirect_to root_path
+    end
+
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body,:post_id,:user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "pages#index"
   resources :posts
   resources :posts do
-    resources :comments, only: [:create]
+    resources :comments, only: [:new, :create]
   end
   devise_for :users
 end
