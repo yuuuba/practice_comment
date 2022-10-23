0c3a7fd406f073fe63b5b899a926c671babd44b8
yuuuba <nsabu45@gmail.com>
2022-10-23 21:06:50 +0900

    リレーション定義 #1

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :user #Comment.userでコメントの所有者を取得
+  belongs_to :post  #Comment.postでそのコメントがされた投稿を取得
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  has_many :comments  #User.commentsで、ユーザーの所有するコメントを取得できる。
 end
